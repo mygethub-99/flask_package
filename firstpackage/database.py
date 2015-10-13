f461662fae23c52825e90e6ce80cfdc24e17b9c0
mygethub-99 <tomakeitcount@gmail.com>
2015-10-12 21:37:59 -0500

    Create database.py

---
+++ firstpackage/database.py
@@ -0,0 +1,12 @@
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from imported import Base, Restaurant, MenuItem
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
