a77f285ddae74cb2248f200229dbd49a4ba18f1c
mygethub-99 <tomakeitcount@gmail.com>
2015-10-12 21:29:32 -0500

    Create runserver.py

---
+++ runserver.py
@@ -0,0 +1,8 @@
+from firstpackage import app
+app.run(debug=True)
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
